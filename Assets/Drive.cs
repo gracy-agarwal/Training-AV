f59709e5d8a1900b5c305c0abe530d61d423612f
mridul-outscal <mridul.agrawal@outscal.com>
2024-07-14 23:50:20 +0530

    refactored Drive.cs

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -1,94 +1,102 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
 public class Drive : MonoBehaviour
 {
-    public float speed = 50.0F;
-    public float rotationSpeed = 100.0F;
-    public float visibleDistance = 200.0F;
+    // Car Movement Data:
+    [SerializeField] private float speed = 50.0F;
+    [SerializeField] private float rotationSpeed = 100.0F;
+    [SerializeField] private float visibleDistance = 200.0F;
+    
+    // Car Training Data:
+    const string trainingDataPath = "/trainingData.txt";
     List<string> collectedTrainingData = new List<string>();
-    StreamWriter tdf;
+    StreamWriter traininDataFile;
 
-    // Start is called before the first frame update
-    void Start()
+    void Start() => CreateTrainingDataFile();
+
+    private void CreateTrainingDataFile()
     {
-        string path = Application.dataPath + "/trainingData.txt";
-        tdf = File.CreateText(path);
+        string fullPath = Application.dataPath + trainingDataPath;
+        traininDataFile = File.CreateText(fullPath);
     }
-
-    void OnApplicationQuit()
-    {
-        foreach(string td in collectedTrainingData)
-        {
-            tdf.WriteLine(td);
-        }
-        tdf.Close();
-
-    }
-
-    float Round(float x)
-    {
-        return(float)System.Math.Round(x, System.MidpointRounding.AwayFromZero) / 2.0f;
-    }
-    // Update is called once per frame
+    
     void Update()
     {
-        float translationInput = Input.GetAxis("Vertical");
-        float rotationInput = Input.GetAxis("Horizontal");
-        float translation = Time.deltaTime * speed * translationInput;
-        float rotation = Time.deltaTime * rotationSpeed * rotationInput;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
-
-        Debug.DrawRay(transform.position, this.transform.forward * visibleDistance, Color.red);
-        Debug.DrawRay(transform.position, this.transform.right * visibleDistance, Color.red);
-
-        //raycasts
-        RaycastHit hit;
-        float fDist = 0, 
-                rDist = 0, 
-                lDist = 0,
-                r45Dist = 0,
-                l45Dist = 0;
-
-        //forward
-        if(Physics.Raycast(transform.position, this.transform.forward, out hit, visibleDistance))
-        {
-            fDist = 1 - Round(hit.distance/visibleDistance);
-        }
-
-        //right
-        if(Physics.Raycast(transform.position, this.transform.right, out hit, visibleDistance))
-        {
-            fDist = 1 - Round(hit.distance/visibleDistance);
-        }
-
-        //left
-        if(Physics.Raycast(transform.position, -this.transform.right, out hit, visibleDistance))
-        {
-            fDist = 1 - Round(hit.distance/visibleDistance);
-        }
-
-        //right 45
-        if(Physics.Raycast(transform.position, Quaternion.AngleAxis(45, Vector3.up) * this.transform.right, out hit, visibleDistance))
-        {
-            fDist = 1 - Round(hit.distance/visibleDistance);
-        }
-
-        //left 45
-        if(Physics.Raycast(transform.position, Quaternion.AngleAxis(45, Vector3.up) * -this.transform.right, out hit, visibleDistance))
-        {
-            fDist = 1 - Round(hit.distance/visibleDistance);
-        }
-
-        string td = fDist + "," + rDist + "," + lDist + "," + r45Dist + "," + l45Dist + "," + Round(translationInput) + "," + Round(rotationInput);
-
-        if(!collectedTrainingData.Contains(td))
-        {
-            collectedTrainingData.Add(td);
-        }
-        
+        float cachedTranslationalInput = HandleTranslationalMovement();
+        float cachedRotationalInput = HandleRotationalMovement();
+        DrawRaycasts(cachedTranslationalInput, cachedRotationalInput);
     }
+    
+    private float HandleTranslationalMovement()
+    {
+        float translationInput = Input.GetAxis("Vertical");
+        float distanceToCover = Time.deltaTime * speed * translationInput;
+        transform.Translate(0, 0, distanceToCover);
+        return translationInput;
+    }
+
+    private float HandleRotationalMovement()
+    {
+        float rotationInput = Input.GetAxis("Horizontal");
+        float angleToRotate = Time.deltaTime * rotationSpeed * rotationInput;
+        transform.Rotate(0, angleToRotate, 0);
+        return rotationInput;
+    }
+    
+    private void DrawRaycasts(float translationInput, float rotationInput)
+    {
+        RecordedData recordedData;
+        recordedData.fDist = RaycastInDirection(transform.forward);
+        recordedData.rDist = RaycastInDirection(transform.right);
+        recordedData.lDist = RaycastInDirection(-transform.right);
+        recordedData.r45Dist = RaycastInDirection(Quaternion.AngleAxis(45, Vector3.up) * transform.forward);
+        recordedData.l45Dist = RaycastInDirection(Quaternion.AngleAxis(-45, Vector3.up) * transform.forward);
+
+        UpdateTrainingData(recordedData, translationInput, rotationInput);
+    }
+
+    private void UpdateTrainingData(RecordedData recordedData, float translationInput, float rotationInput)
+    {
+        string trainingData = 
+            recordedData.fDist + "," + 
+            recordedData.rDist + "," + 
+            recordedData.lDist + "," + 
+            recordedData.r45Dist + "," + 
+            recordedData.l45Dist + "," + 
+            Round(translationInput) + "," + 
+            Round(rotationInput);
+
+        if(!collectedTrainingData.Contains(trainingData))
+            collectedTrainingData.Add(trainingData);
+    }
+
+    private float RaycastInDirection(Vector3 direction)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, visibleDistance))
+            return 1 - Round(hit.distance / visibleDistance);
+        return 0;
+    }
+
+    float Round(float x) => (float)System.Math.Round(x * 2, System.MidpointRounding.AwayFromZero) / 2.0f;
+    
+    void OnApplicationQuit() => SaveTrainingData();
+
+    private void SaveTrainingData()
+    {
+        foreach(string data in collectedTrainingData)
+            traininDataFile.WriteLine(data);
+        traininDataFile.Close();
+    }
+}
+
+public struct RecordedData
+{
+    public float fDist;
+    public float rDist;
+    public float lDist;
+    public float r45Dist;
+    public float l45Dist;
 }
