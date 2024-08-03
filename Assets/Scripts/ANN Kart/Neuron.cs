4c1b631ff961bb14cfd4220bd0ed5e0e71044a72
mridul-outscal <mridul.agrawal@outscal.com>
2024-08-04 00:23:08 +0530

    refactored Neuron.cs

--- Assets/Scripts/ANN Kart/Neuron.cs
+++ Assets/Scripts/ANN Kart/Neuron.cs
@@ -1,24 +1,42 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Neuron {
 
-	public int numInputs;
-	public double bias;
-	public double output;
-	public double errorGradient;
-	public List<double> weights = new List<double>();
-	public List<double> inputs = new List<double>();
+	private int numberOfInputs;
+	private float bias;
+	private float output;
+	private List<float> weights;
+	private List<float> inputs;
 
-	public Neuron(int nInputs)
+	public Neuron(int numberOfInputs)
 	{
-		float weightRange = (float) 2.4/(float) nInputs;
-		bias = UnityEngine.Random.Range(-weightRange,weightRange);
-		numInputs = nInputs;
-
-		for(int i = 0; i < nInputs; i++)
-			weights.Add(UnityEngine.Random.Range(-weightRange,weightRange));
+		InitializeVariables(numberOfInputs);
+		InitializeWeights();
+		InitializeBias();
 	}
+
+	private void InitializeVariables(int numberOfInputs)
+	{
+		this.numberOfInputs = numberOfInputs;
+		weights = new List<float>();
+		inputs = new List<float>();
+	}
+	
+	private void InitializeBias() => bias = Random.Range(-1f,1f);
+	
+	private void InitializeWeights()
+	{
+		float weightRange = CalculateWeightRange();
+		
+		for(int i = 0; i < numberOfInputs; i++)
+			weights.Add(Random.Range(-weightRange,weightRange));
+	}
+
+	/// <summary>
+	/// This function calculates a weight range based on the number of inputs to the neuron.
+	/// This calculation is intended for weight initialization using a heuristic similar to Xavier initialization, 
+	/// </summary>
+	/// <returns></returns>
+	private float CalculateWeightRange() => (1.0f / Mathf.Sqrt(numberOfInputs));
 }
