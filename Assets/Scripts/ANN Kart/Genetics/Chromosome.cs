87dffe5041e8711bcf2477e50679d41b17edcbc2
mridul-outscal <mridul.agrawal@outscal.com>
2024-08-11 14:20:38 +0530

    refactored Chromosome.cs

--- Assets/Scripts/ANN Kart/Genetics/Chromosome.cs
+++ Assets/Scripts/ANN Kart/Genetics/Chromosome.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 public class Chromosome
 {
     private List<float> genes;
-
     public int Length => genes.Count;
 
-    public Chromosome(int length) => genes = new List<float>(length);
+    public Chromosome(int length) => InitializeGenes(length);
 
+    private void InitializeGenes(int length) => genes = new List<float>(length); 
+    
     public void SetGeneAtPosition(int position, float value) => genes[position] = value;
 
     public float GetGeneAtPosition(int position) => genes[position];
